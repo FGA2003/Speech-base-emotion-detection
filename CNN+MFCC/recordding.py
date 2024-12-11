e8ac3af8d67e30401c0b1eeadee959e90818bf15
FGA2003 <153610191+FGA2003@users.noreply.github.com>
2024-12-11 22:08:31 +0000

    Delete CNN+MFCC directory

--- CNN+MFCC/recordding.py
+++
@@ -1,5 +0,0 @@
-import sounddevice as sd
-from scipy.io.wavfile import writefs = 44100  # sample rate
-seconds = 4  # duration
-sd.wait()  # until finish
-write('output.wav', fs, myrecording)  # save 
